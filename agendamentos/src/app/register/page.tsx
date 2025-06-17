fd9899a3a9ec18b79ae6c6a9772f0131718fb9c6
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2025-06-17 18:05:57 -0300

    update start styles

--- agendamentos/src/app/register/page.tsx
+++ agendamentos/src/app/register/page.tsx
@@ -1,7 +1,17 @@
 export default function Register(){
     return(
-        <div>
-            <h1>Registre-se</h1>
+        <div className="flex flex-col h-screen items-center justify-center">            
+            <div className="flex flex-col items-center gap-2 border-2">
+                <h1>Registre-se</h1>
+                <div className="flex gap-2">               
+                    <label htmlFor="email">Email</label>
+                    <input type="text" placeholder="email" id="email"/>
+                </div>
+                <div className="flex gap-2">
+                    <label htmlFor="password">Password</label>
+                    <input type="text" placeholder="password" id="password"/>
+                </div>
+            </div>
         </div>
     )
 }
