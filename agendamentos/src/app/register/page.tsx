fd743d25a88b48bc10aaa50e8fa0c0c120b77052
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2025-06-18 11:16:30 -0300

    update forms in register

--- agendamentos/src/app/register/page.tsx
+++ agendamentos/src/app/register/page.tsx
@@ -1,17 +1,12 @@
+import { InputForm } from "../form/page";
+
 export default function Register(){
     return(
-        <div className="flex flex-col h-screen items-center justify-center">            
-            <div className="flex flex-col items-center gap-2 border-2">
-                <h1>Registre-se</h1>
-                <div className="flex gap-2">               
-                    <label htmlFor="email">Email</label>
-                    <input type="text" placeholder="email" id="email"/>
-                </div>
-                <div className="flex gap-2">
-                    <label htmlFor="password">Password</label>
-                    <input type="text" placeholder="password" id="password"/>
-                </div>
-            </div>
+        <div className="flex flex-col h-screen items-center justify-center">
+            <div className="flex items-center justify-center w-96">
+                <InputForm />
+            </div>            
+            
         </div>
     )
 }
